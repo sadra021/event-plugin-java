5bbd1821ee56234a98c889b7701506bb735a262b
sadra <sadrazx0241>
2023-03-05 12:24:28 +0330

    custom log first layer

--- plugins/mongodbplugin/src/main/java/org/tron/mongodb/MongoManager.java
+++ plugins/mongodbplugin/src/main/java/org/tron/mongodb/MongoManager.java
@@ -70,11 +70,14 @@ public class MongoManager {
     }
 
     public boolean isExistPublicAddress(String address) {
+
+        log.info("Custom log : recived to isExistPublicAddress method ==> {} ", address);
+
         MongoCollection<Document> collection = db.getCollection("publicaddresses");
         Bson filter = Filters.eq("address", address);
 
         Document doc = (Document) collection.find(filter);
-        log.info("Custom log : recived to isExistPublicAddress method ==> {} ", address);
+        log.info("Custom log : return stage {} ", address);
         return doc.isEmpty();
     }
 
